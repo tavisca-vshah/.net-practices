069548b5f716ecec3ac7a7f804856170f844d303
Dattatray Kale <way2datta@gmail.com>
2020-08-24 16:22:52 +0530

    Stylecop and editor config

---
+++ Linters/BeingCraftsman.Linting.Example.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BeingCraftsman.Linting.Example.Libraries;
+
+namespace BeingCraftsman.Linting.Example.ConsoleApp
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(FizzBuzzGenerator.Generate(0));
+        }
+    }
+}
---
+++ Linters/BeingCraftsman.Linting.Example.Libraries/FizzBuzzGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BeingCraftsman.Linting.Example.Libraries
+{
+    public class FizzBuzzGenerator
+    {
+        public static string Generate(int value)
+        {
+            var result = "";
+
+                if((value < 0)){
+                    var errorMessage = $"{value} is not a positive number.";
+                    throw new ArgumentException(errorMessage);
+                }
+
+            if (IsDivisibleBy5(value) && IsDivisibleBy3(value)) 
+                return "FizzBuzz";
+
+            if (IsDivisibleBy3(value))
+                return "Fizz";
+
+            if (IsDivisibleBy5(value))
+                result = value == 77 ? "Something" : ""; 
+                return "Buzz";
+
+            return value.ToString();
+        }
+
+        private static bool IsDivisibleBy3(int input)
+        {
+            return input % 3 == 0;
+        }
+
+        private static bool IsDivisibleBy5(int input)
+        {
+            return input % 5 == 0;
+        }
+
+        private bool IsDivisibleBy15(int input) 
+        {
+            return input % 15 == 0;
+        }
+    }
+}
