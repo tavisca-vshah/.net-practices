ccba8068137736508e22ee95f4f33844ba4c5570
Dattatray Kale <way2datta@gmail.com>
2020-08-28 21:54:12 +0530

    Added a characterizarion tests

---
+++ MovieRental/MovieRental.Tests/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieRental.ClassLibrary;
+using Xunit;
+
+namespace MovieRental.Tests
+{
+    public class MovieRentalTests
+    {
+        [Fact]
+        public void Customer_GenerateTextStatement()
+        {
+            CustInfo customer = new CustInfo("John Doe");
+
+            customer.addRental(new RentalDetails(new MovieData("Harry Potter", 0), 4));
+            customer.addRental(new RentalDetails(new MovieData("John Wick", 2), 10));
+            customer.addRental(new RentalDetails(new MovieData("The Boult", 1), 20));
+
+            string statement = customer.statement();
+
+            Assert.Equal("Rental Record for John Doe\n" +
+                    "\tHarry Potter\t5\n" +
+                    "\tJohn Wick\t12\n" +
+                    "\tThe Boult\t60\n" +
+                    "Amount owed is 77\n" +
+                    "You earned 4 frequent renter points", statement);
+        }
+    }
+}
